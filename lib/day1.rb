1ad1989136ee9e98827559f766fd47e35616a170
David Bull <david@uk-dave.com>
2021-12-03 10:54:23 +0000

    Refactor Day 1 to use .count instead of .sum

--- lib/day1.rb
+++ lib/day1.rb
@@ -2,7 +2,7 @@
 
 class Day1
   def count_increases depths
-    depths.each_cons(2).sum {|(depth1, depth2)| depth2 > depth1 ? 1 : 0 }
+    depths.each_cons(2).count {|(depth1, depth2)| depth2 > depth1 }
   end
 
   def count_increases_sliding_window depths
